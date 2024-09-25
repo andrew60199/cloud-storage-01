bcc0acc9f2e8628dbfb0815d0a70db6cf1bfe6c3
AND.EVV <andrew60199@gmail.com>
2024-09-25 11:39:37 +1000

    Add /api/user/login route

---
+++ server/controllers/api/userRoutes.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const router = require('express').Router()
+const { sequelize, User } = require('../../db/models')
+
+// api/user
+
+router.post('/login', async (req, res) => {
+  const failMessage = 'Incorrect email or password'
+
+  if (!req.body.email) return res.status(400).json({ message: 'Please enter a valid email address' })
+  if (!req.body.password) return res.status(400).json({ message: 'Please enter a valid password' })
+
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } })
+    if (!userData) return res.status(400).json({ message: failMessage })
+
+    const validPassword = await userData.checkPassword(req.body.password)
+    if (!validPassword) return res.status(400).json({ message: failMessage })
+
+    const userPlain = userData.get({ plain: true })
+    // console.log(userPlain)
+
+    const { password, deleted_at, ...rest } = userPlain
+
+    return res.status(200).json({ message: 'Welcome back!', user: rest })
+    
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({ message: failMessage })
+  }  
+})
+
+router.post('/logout', async (req, res) => {
+  
+})
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const { sequelize } = require('./db/models')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
-// const routes = require('./controllers')
+const routes = require('./controllers')
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -13,7 +13,7 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// app.use(routes)
+app.use(routes)
 
 const connectDb = async () => {
     try {
